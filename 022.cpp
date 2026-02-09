46637057ecb9182dc9515c1dad46bb581b6d1692
Koichiro <tokails188s@gmail.com>
2026-02-09 23:06:42 +0900

    ac

--- 022.cpp
+++ 022.cpp
@@ -5,5 +5,12 @@ long long a,b,c;
 int main() {
     cin >> a >> b >> c;
 
+    long long result = 0;
+    long long gcd = __gcd(__gcd(a,b),c);
 
+    result += (a / gcd) - 1;
+    result += (b / gcd) - 1;
+    result += (c / gcd) - 1;
+    cout << result << endl;
+    return 0;
 }
