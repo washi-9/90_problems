79bd0bf3c3f0cdecd85d3a9896258794fa4dd72e
Koichiro <tokails188s@gmail.com>
2026-03-14 21:09:42 +0900

    ac

--- 003.cpp
+++ 003.cpp
@@ -1,67 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/* グラフを表すデータ型 */
-using Graph = vector<vector<int>>;
+int MAX_DEPTH = -1;
+int DEPTH_V = -1;
 
-/* 頂点 s から DFS(ここではスタックを使う) */
-vector<int> dfs(const Graph &G, int s) {
-    int N = G.size();
-
-    /* 頂点 s からの距離 */
-    vector<int> dist(N, -1);
-    dist[s] = 0;
-
-    /* スタックで DFS */
-    stack<int> st({s});
-    while (!st.empty()) {
-        int v = st.top();
-        st.pop();
-        for (auto nv: G[v]) {
-            if (dist[nv] == -1) {
-                st.push(nv);
-                dist[nv] = dist[v] + 1;
-            }
-        }
+void dfs(int v,int p, int d, const vector<vector<int>>& g) {
+    if (d > MAX_DEPTH) {
+        MAX_DEPTH = d;
+        DEPTH_V = v;
     }
-
-    /* リターン */
-    return dist;
+    for(int next_v : g[v]) {
+        if (next_v == p) continue;
+        dfs(next_v, v, d + 1, g);
+    }
 }
 
-int main(void) {
-    /* 入力 */
-    int N;
-    cin >> N;
-    Graph G(N);
-    for (int i = 0; i < N-1; i++) {
-        int a, b; cin >> a >> b; a--, b--;
-        /* 無向グラフを作成 */
-        G[a].push_back(b);
-        G[b].push_back(a);
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n+1);
+    for (int i=0;i<n-1;i++) {
+        int a,b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
 
-    /* 頂点 0 から DFS */
-    auto dist0 = dfs(G, 0);
-
-    /* 距離最大の点を求める */
-    int mx = -1, mv = -1;
-    for (int v = 0; v < N; v++) {
-        if (mx < dist0[v]) {
-            mx = dist0[v];
-            mv = v;
-        }
-    }
-
-    /* 頂点 mv から DFS */
-    auto distmv = dfs(G, mv);
-
-    /* その最大値を求める */
-    mx = -1;
-    for (int v = 0; v < N; v++) {
-        mx = max(mx, distmv[v]);
-    }
-    cout << mx + 1 << endl;
-    
+    int max_score = -1;
+    dfs(1, -1, 0, g);
+    MAX_DEPTH = 0;
+    dfs(DEPTH_V,-1, 0,g);
+    cout << MAX_DEPTH+1 << endl;
     return 0;
 }
